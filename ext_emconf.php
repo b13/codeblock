09444c3910c99fd78385e487aec550b2883c19b4
Andreas Hämmerl <andreas.haemmerl@b13.com>
2019-08-29 09:46:01 +0200

    [TASK] Initial commit
    Relates to: BEXT-114

---
+++ ext_emconf.php
@@ -0,0 +1,15 @@
+<?php
+
+$EM_CONF['codeblock'] = [
+    'title' => 'b13 Codeblock',
+    'description' => 'b13 content element for text with code highlighting.',
+    'category' => 'fe',
+    'author' => 'Andreas Hämmerl',
+    'author_email' => 'typo3@b13.com',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author_company' => 'b13 GmbH, Stuttgart',
+    'version' => '0.0.1'
+];
