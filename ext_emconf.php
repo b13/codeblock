2a109fe9a84122d3c48346da560824852535350f
Georg Ringer <georg.ringer@gmail.com>
2020-09-16 19:29:58 +0200

    [TASK] Improve ext_emconf
    - Remove not needed info
    - use

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,18 @@
 <?php
 
-$EM_CONF['codeblock'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Content Type "Code Block"',
     'description' => 'Content type for displaying source code with syntax highlighting.',
     'category' => 'fe',
     'author' => 'Andreas Hämmerl, David Steeb',
     'author_email' => 'typo3@b13.com',
+    'author_company' => 'b13 GmbH',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author_company' => 'b13 GmbH, Stuttgart',
+    'clearCacheOnLoad' => true,
     'version' => '1.3.4',
     'constraints' => [
-            'depends' => [
-                'typo3' => '9.5.99-10.99.99',
-            ],
+        'depends' => [
+            'typo3' => '9.5.0-10.4.99',
         ],
+    ],
 ];
