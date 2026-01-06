a373a30102431e8d1a0e0188c7c2554236b05043
Benni Mack <benni@typo3.org>
2026-01-06 16:02:35 +0100

    [TASK] Add support for TYPO3 v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-13.99.99',
+            'typo3' => '10.4.0-14.99.99',
         ],
     ],
 ];
